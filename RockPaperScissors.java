999a013091d2193b7f8000daca3de19523394b19
JamesSmith232 <80684500+JamesSmith232@users.noreply.github.com>
2022-05-26 09:51:08 -0500

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 
 public class RockPaperScissors {
   
+    //Globally declared variables 
     final static int ROCK = 0; 
     final static int PAPER = 1; 
     final static int SCISSORS = 2;
@@ -28,22 +29,27 @@ public class RockPaperScissors {
         System.out.println("This program will allow you to play a game of Rock, Paper, Scissors with the computer.");
         System.out.println("\n========================================================================================\n");
         
+        //Getting the number of rounds
         System.out.println("How many rounds do you need to win the game: ");
         gameNum = keyboard.nextInt();
         
         System.out.println("\nEach item has a number attached. You will need to use that number to choose that item.\n"); 
-        gameTime(gameNum);
+            
+        //passing the gameNum varaible to play the game  
+        gameTime(gameNum); 
         results(); 
     }
     
     public static int computerSelection() {
         double random; 
         
+        //Using Math.random and multiplying that number to get in specific range 
         random = Math.random(); 
         random *= 3; 
         return (int)random;
     }
         
+    //if statements to decide the winner of the round 
     public static int game(int user, int computer) {
         if (computer == ROCK) { 
             if (user == PAPER) return USERWIN;
@@ -72,6 +78,7 @@ public class RockPaperScissors {
         String userLose = "You lost the round."; 
         String userTie = "You and the computer tied."; 
         
+        //while loop to decide the winner of the game 
         while (userCount < gameNum && computerCount < gameNum) {
             System.out.println(userSelection);
             user = keyboard.nextInt();
@@ -105,6 +112,7 @@ public class RockPaperScissors {
         }
     }
     
+    //Using the globally declared variables to display the results
     public static void results() {
         if (userCount > computerCount) {
             System.out.println("Congrats! You beat the computer with the score of " +userCount+ " to " +computerCount);
